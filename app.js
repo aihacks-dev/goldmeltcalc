395659bfb3bbf4a2c23d2a0dc240498556b45129
aihacks-dev <aihacks411@gmail.com>
2026-01-31 15:32:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "v4";
+const APP_VERSION = "v6";
 
 // Pre-1933 US gold approximate AGW (troy oz)
 const PRE33 = [
@@ -155,3 +155,4 @@ const versionEl = document.getElementById("version");
 if (versionEl) {
   versionEl.textContent = APP_VERSION;
 }
+
