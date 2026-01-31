5b9561e670bf7c03a292a9f7b02045fa99b28ee8
aihacks-dev <aihacks411@gmail.com>
2026-01-31 15:37:24 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gold-melt-pwa-v6";
+const CACHE = "gold-melt-pwa-v7";
 const ASSETS = [
   "./",
   "./index.html",
@@ -41,6 +41,7 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
 
 
 
